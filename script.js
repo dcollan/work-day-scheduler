703283e471af7b4d670ae8f32996e7759a8db0ad
Danny Collan <collan97@gmail.com>
2023-09-10 13:09:24 -0700

    Dynamically added textarea and savebutton features

--- script.js
+++ script.js
@@ -10,27 +10,37 @@ $(document).ready(function () {
     $("#currentDay").text(currentDate);
   }
 
-  // Function to set timeblocks
+  // Function to create timeblocks
   function setTimeBlocks() {
     // Utilize day.js feature to implement the hour
     var currentHour = dayjs().hour();
 
     // Loop through each hour from 9am - 5pm (24 hour time: 9 - 17)
     for (var hour = 9; hour <= 17; hour++) {
-      // Initialize time block variable. Referred to example HTML code to create elements dynamically 
+      // Initialize time block variable 
+      // Per all '.addClass' elements, referred to example HTML code to create elements dynamically 
       var timeBlock = $("<div>").addClass("row time-block");
 
       // Initialize time label variable per time block
       var timeLabel = $("<div>")
         .addClass("col-2 col-md-1 hour text-center py-3")
         .text(dayjs().hour(hour).format("hA"));
+
+      // Create textarea element for user to enter things to keep tracked
+      var enterText = $("<textarea>")
+        .addClass("col-8 col-md-10 description")
+        .attr("id", "hour-" + hour);
+
+      // Create 'save button' element for saving information entered in textarea
+      var save = $("<button>")
+        .addClass("btn saveBtn col-2 col-md-1")
+        .attr("aria-label", "save")
+        .append($("<i>").addClass("fas fa-save").attr("aria-hidden", "true"));
     }
   }
-
   // Call setCurrentDate function
   setCurrentDate();
 
   // Call the function to create the timeblocks and initialize the page
   setTimeBlocks();
-
 });
