6648c5ca8b28efe72c572c469b6ad849cab24adc
Danny Collan <collan97@gmail.com>
2023-09-10 12:38:38 -0700

    added 'setTimeBlocks' function, initialized timeblocks & timelabels

--- script.js
+++ script.js
@@ -9,6 +9,28 @@ $(document).ready(function () {
     // Use 'currentDay' ID element to display the date
     $("#currentDay").text(currentDate);
   }
+
+  // Function to set timeblocks
+  function setTimeBlocks() {
+    // Utilize day.js feature to implement the hour
+    var currentHour = dayjs().hour();
+
+    // Loop through each hour from 9am - 5pm (24 hour time: 9 - 17)
+    for (var hour = 9; hour <= 17; hour++) {
+      // Initialize time block variable. Referred to example HTML code to create elements dynamically 
+      var timeBlock = $("<div>").addClass("row time-block");
+
+      // Initialize time label variable per time block
+      var timeLabel = $("<div>")
+        .addClass("col-2 col-md-1 hour text-center py-3")
+        .text(dayjs().hour(hour).format("hA"));
+    }
+  }
+
   // Call setCurrentDate function
   setCurrentDate();
+
+  // Call the function to create the timeblocks and initialize the page
+  setTimeBlocks();
+
 });
